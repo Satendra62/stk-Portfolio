12d9ece6067a167deebe8172b3b404261d3e3df6
SATENDRA KUMAR <satendra62060@gmail.com>
2025-04-12 13:28:06 +0530

    Add CV

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
 
         {/* CV Button */}
         <button className="hidden md:block bg-orange-500 text-white px-4 py-2 rounded-md hover:bg-orange-600 transition">
-          MY CV
+          <a href="https://drive.google.com/file/d/1BwWNUm8uoGfyk5-bbKbEHWuZfXEzpnp1/view?usp=drive_link">MY CV</a>
         </button>
       </span>
 
